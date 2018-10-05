58196294db58cac7655b62021afcf6d5ec8378e1
Dirk Wulff <dirk.wulff@gmail.com>
2018-10-05 18:16:11 +0200

    first day done

--- _sessions/IntroToR/IntroToR_interactive.R
+++ _sessions/IntroToR/IntroToR_interactive.R
@@ -1,6 +1,6 @@
 # R Studio Settings ----- 
 # 1.  Panel icon -> switch to Console on right
-# 2.  Go to preferences and switch of workspace storing
+# 2.  Go to preferences and switch off workspace saving
 
 # Create Project -----
 # 1.  Create project named BaselRBootcamp
@@ -9,53 +9,33 @@
 
 # R as a calculator ----- 
 
-# 1.  In console
+# 1. In console
+# 1.1 Calculate 2 + 2
+# 1.2 Create objects via assignment
+# 1.3 Print result
+# 1.4 Use result
+# 1.5 Re-evalute result
+# 1.6 Overwrite result
 
-# Calculate 2 + 2
-
-
-# Create objects via assignment
-
-
-# Print result
-
-# Use result
-
-# Reevalute result
-
-# Overwrite result
-
-# 1.  In script
-# 1.1 Open and save new script
-# 1.2 Repeat stuff by sending it to console
-
-# Calculate 2 + 2
-
-# Create results objects via assignment
-
-# Print result
-
-# Use result: add 2
-
-# Reevalute result
-
-# Overwrite result
+# 2.  In script
+# 2.1 Open and save new script
+# 2.2 Repeat stuff by sending it to console
+# 2.2.1 Calculate 2 + 2
+# 2.2.2 Create objects via assignment
+# 2.2.3 Print result
+# 2.2.4 Use result: add 2
+# 2.2.5 Reevalute result
+# 2.2.6 Overwrite result
 
 # Functions in R ----- 
 
-# Create a numeric vector called age
-
-# Ccalculate mean age (auto-complete)
-
-# Store result
-
-# Add NA to age
-
-# Compute mean
-
-# Check help
-
-# Use n.rm argument
+# 1 Create a numeric vector called age
+# 2. Ccalculate mean age (auto-complete)
+# 3. Store result
+# 4. Add NA to age
+# 5. Compute mean
+# 6. Check help
+# 7. Use n.rm argument
 
 # Close RStudio & Reopen ----
 
